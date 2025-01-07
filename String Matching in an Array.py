edc79913c740b78fceb109acc492e585f1b9bbfe
ganesh <ganeshnehe07@gmail.com>
2025-01-07 23:00:15 +0530

    String Matching in an Array Update

--- String Matching in an Array.py
+++ String Matching in an Array.py
@@ -1,7 +1,7 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         result = []
-        for i in range(len(words)):
+        for i in range(len(words)):      
             for j in range(len(words)):
                 if i != j and words[i] in words[j]:
                     result.append(words[i])
