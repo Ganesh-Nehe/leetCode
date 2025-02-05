a764f9b46f687118a9c9e7d4e9825ebdd6b131b4
ganesh <ganeshnehe07@gmail.com>
2025-02-05 22:14:51 +0530

    Check if One String Swap Can Make Strings Equal update0

--- Check if One String Swap Can Make Strings Equal.py
+++ Check if One String Swap Can Make Strings Equal.py
@@ -3,5 +3,5 @@ class Solution:
         if s1 == s2:
             return True
         diff = [(a, b) for a, b in zip(s1, s2) if a != b]
-
+ 
         return len(diff) == 2 and diff[0] == diff[1][::-1]
