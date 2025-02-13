da3bf28882ded489a3a03b44c441a0cacfe53257
ganesh <ganeshnehe07@gmail.com>
2025-02-13 23:28:35 +0530

    Minimum Operations to Exceed Threshold Value II

---
+++ Minimum Operations to Exceed Threshold Value II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        heapify(nums) 
+        operations = 0
+        
+        while nums[0] < k:
+            x = heappop(nums) 
+            y = heappop(nums) 
+            new_value = min(x, y) * 2 + max(x, y)  
+            heappush(nums, new_value)  
+            operations += 1  
+        
+        return operations
